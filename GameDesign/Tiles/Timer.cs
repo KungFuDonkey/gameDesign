3333fb38c640ffa2c7048780498097039faf5e07
Sietze <sietze.riemersma@gmail.com>
2020-02-26 14:57:11 +0100

    Timer readded

---
+++ GameDesign/Tiles/Timer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDesign
+{
+    public class Timer
+    {
+        long currentTime_ms;
+        long phaseStartTime_ms;
+        long phaseTime_ms;
+
+        Timer()
+        {
+            currentTime_ms = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            phaseStartTime_ms = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            phaseTime_ms = 1000 * 20;
+        }
+
+        public bool isPhaseOver()
+        {
+            updateCurrentTime();
+            if (currentTime_ms > phaseStartTime_ms + phaseTime_ms)
+            {
+                phaseStartTime_ms = currentTime_ms;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void setPhaseTime(long newPhaseTime_ms)
+        {
+            phaseTime_ms = newPhaseTime_ms;
+        }
+
+        private void updateCurrentTime()
+        {
+            currentTime_ms = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        }
+
+    }
+}
