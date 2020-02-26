606d670aa5244f23ae5ca745d3603a430d0584b0
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2020-02-26 14:56:00 +0100

    added dayPhase enum

--- GameDesign/Timer.cs
+++ GameDesign/Timer.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace GameDesign
 {
+    public enum dayPhase
+    {
+        morning,
+        afternoon,
+        night
+    }
+
     public class Timer
     {
         long currentTime_ms;
